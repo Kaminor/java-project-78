9a787d01c61978a870f797daacd0437b9d316980
Ivan Zubarev <swim_zb@mail.ru>
2025-09-18 08:53:33 +0300

    create project and add dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("java")
+    checkstyle
+    application
+    id("org.sonarqube") version "6.2.0.5505"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.3")
+    implementation ("info.picocli:picocli:4.7.7")
+    compileOnly ("org.projectlombok:lombok:1.18.32")
+    annotationProcessor ("org.projectlombok:lombok:1.18.32")
+    testCompileOnly ("org.projectlombok:lombok:1.18.32")
+    testAnnotationProcessor ("org.projectlombok:lombok:1.18.32")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Kaminor_java-project-78")
+        property("sonar.organization", "kaminor")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
