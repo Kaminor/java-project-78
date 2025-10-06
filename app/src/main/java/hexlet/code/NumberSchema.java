3b7541dcc53ccda8463d17a9ec16f9e07f1f0667
Ivan Zubarev <swim_zb@mail.ru>
2025-10-06 20:41:56 +0300

    add number schema

--- app/src/main/java/hexlet/code/NumberSchema.java
+++ app/src/main/java/hexlet/code/NumberSchema.java
@@ -1,13 +1,10 @@
 package hexlet.code;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.function.Predicate;
+public class NumberSchema extends BaseSchema<Integer> {
 
-public class NumberSchema {
-    private final Map<String, Predicate<Integer>> rules = new LinkedHashMap<>();
-
-    public NumberSchema() { }
+    public NumberSchema() {
+        super();
+    }
 
     public NumberSchema required() {
         rules.put("required", r -> r != null);
@@ -22,24 +19,5 @@ public class NumberSchema {
     public NumberSchema range(int min, int max) {
         rules.put("range", r -> r >= min && r <= max);
         return this;
-    }
-
-    public boolean isValid(Object value) {
-        if (value == null) {
-            return !rules.containsKey("required");
-        }
-
-        if (!(value instanceof Integer)) {
-            return false;
-        }
-
-        var num = (Integer) value;
-
-        for (Predicate<Integer> predicate : rules.values()) {
-            if (!predicate.test(num)) {
-                return false;
-            }
-        }
-        return true;
     }
 }
