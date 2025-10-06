06c75011c64e2d02489ac28d431ea11f2d04d3b5
Ivan Zubarev <swim_zb@mail.ru>
2025-10-06 20:43:44 +0300

    add number schema test and fix build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     checkstyle
     id("org.sonarqube") version "6.3.1.5724"
+    jacoco
 }
 
 group = "hexlet.code"
@@ -15,15 +16,15 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.3")
-    implementation ("info.picocli:picocli:4.7.7")
-    compileOnly ("org.projectlombok:lombok:1.18.32")
-    annotationProcessor ("org.projectlombok:lombok:1.18.32")
-    testCompileOnly ("org.projectlombok:lombok:1.18.32")
-    testAnnotationProcessor ("org.projectlombok:lombok:1.18.32")
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
 }
 
 sonar {
