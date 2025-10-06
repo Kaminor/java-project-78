a3ec2fd2bbc2fe9cc1b01982f8563c7d70222d02
Ivan Zubarev <swim_zb@mail.ru>
2025-10-06 20:42:37 +0300

    change string schema for base schema

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,13 +1,10 @@
 package hexlet.code;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.function.Predicate;
+public class StringSchema extends BaseSchema<String> {
 
-public class StringSchema {
-    private final Map<String, Predicate<String>> rules = new LinkedHashMap<>();
-
-    public StringSchema() { }
+    public StringSchema() {
+        super();
+    }
 
     public StringSchema required() {
         rules.put("required", r -> r != null && !r.trim().isEmpty());
@@ -24,7 +21,10 @@ public class StringSchema {
         return this;
     }
 
-    public boolean isValid(Object value) {
+
+
+
+    /*public boolean isValid(Object value) {
         if (value == null) {
             return !rules.containsKey("required");
         }
@@ -39,55 +39,6 @@ public class StringSchema {
             if (!predicate.test(str)) {
                 return false;
             }
-        }
-        return true;
-    }
-
-
-
-
-
-    /*private boolean required = false;
-    private Integer minLength = null;
-    private String contains = null;
-
-    public StringSchema() {}
-
-    public StringSchema required() {
-        this.required = true;
-        return this;
-    }
-
-    public StringSchema minLength(int length) {
-        this.minLength = length;
-        return this;
-    }
-
-    public StringSchema contains(String subStr) {
-        this.contains = subStr;
-        return this;
-    }
-
-    public boolean isValid(Object value) {
-        if (value == null) {
-            return !required;
-        }
-
-        if (!(value instanceof String)) {
-            return false;
-        }
-
-        String str = (String) value;
-        if (required && str.trim().isEmpty()) {
-            return false;
-        }
-
-        if (minLength != null && str.length() <= minLength) {
-            return false;
-        }
-
-        if (contains != null && !str.contains(contains)) {
-            return false;
         }
         return true;
     }*/
